e3cea565851fb7ce8260fc4dcbab867233bc529e
Mahmoud Zain <zainmahmoud719@gmail.com>
2026-09-02 14:00:00 +0300

    chore: initialize project structure

---
+++ src/main/java/com/mahmoudzain/mnp_porting_system/MnpPortingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mahmoudzain.mnp_porting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnpPortingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnpPortingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahmoudzain/mnp_porting_system/MnpPortingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoudzain.mnp_porting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnpPortingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
